8ddf12fdff0488ebc0e8df9a9de9fbec8d05eafd
Dr. Cyril Robinson Azariah J <cyrilrobinson_ece@sirmvit.edu>
2026-03-08 14:30:05 +0530

    Reorganized repository structure for MSc thesis project

--- test_mediapipe_ok.py
+++
@@ -1,7 +0,0 @@
-import mediapipe as mp
-
-print("MediaPipe version:", mp.__version__)
-print("Has solutions:", hasattr(mp, "solutions"))
-
-fm = mp.solutions.face_mesh.FaceMesh(refine_landmarks=True)
-print("FaceMesh initialized successfully")
--- test_mp_fix.py
+++
@@ -1,4 +0,0 @@
-from mediapipe.python.solutions import face_mesh
-
-fm = face_mesh.FaceMesh(refine_landmarks=True)
-print("FaceMesh initialized successfully")
